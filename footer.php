4b6e0ca7383e8d6d8283d707d9c1700009354367
neha dudhat <84699290+dudhatneha@users.noreply.github.com>
2024-05-24 12:24:28 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,157 @@
+<footer class="footer-top-area pt-100 pb-70">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-sm-6 col-md-6">
+						<div class="single-widget">
+							<img src="assets/img/white-logo.png" alt="White Logo">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt consectetur adipiscing.</p>
+							<form class="newsletter-form" data-toggle="validator">
+								<input type="email" class="form-control" placeholder="Your email" name="EMAIL" required autocomplete="off">
+	
+								<button class="default-btn" type="submit">
+									Subscribe
+								</button>
+	
+								<div id="validator-newsletter" class="form-result"></div>
+							</form>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6 col-md-6">
+						<div class="single-widget single-widget-1">
+							<h3>Industries</h3>
+							<ul>
+								<li>
+									<a href="#">Industries</a>
+								</li>
+								<li>
+									<a href="#">Electronic Materials</a>
+								</li>
+								<li>
+									<a href="#">Gifts & Apparel</a>
+								</li>
+								<li>
+									<a href="#">Auto Parts</a>
+								</li>
+								<li>
+									<a href="#">Power Systems</a>
+								</li>
+								<li>
+									<a href="#">Building Management</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6 col-md-6">
+						<div class="single-widget single-widget-2">
+							<h3>Solutions</h3>
+							<ul>
+								<li>
+									<a href="#">Solutions</a>
+								</li>
+								<li>
+									<a href="#">Automotive Manufacturing</a>
+								</li>
+								<li>
+									<a href="#">Mechanical Engineering</a>
+								</li>
+								<li>
+									<a href="#">Industrial Construction</a>
+								</li>
+								<li>
+									<a href="#">Bridge Construction</a>
+								</li>
+								<li>
+									<a href="#">Oil & Gas Energy</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6 col-md-6">
+						<div class="single-widget single-widget-3">
+							<h3>Company</h3>
+							<ul>
+								<li>
+									<a href="#">About</a>
+								</li>
+								<li>
+									<a href="#">Approach</a>
+								</li>
+								<li>
+									<a href="#">Sustainability</a>
+								</li>
+								<li>
+									<a href="#">News</a>
+								</li>
+								<li>
+									<span>( Emergency Call )</span>
+									<a href="#">
+										<i class="flaticon-telephone-handle-silhouette"></i>
+										(002) 01061245741
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- End Footer Top Area -->
+
+		<!-- Start Footer Bottom Area -->
+		<footer class="footer-bottom-area">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4">
+						<div class="single-widgets">
+							<ul class="terms">
+								<li>
+									<a href="#">Terms & Conditions</a>
+								</li>
+								<li>
+									<a href="#">Privacy Policy</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="single-widgets">
+							<p>
+								Copyright <i class="bx bx-copyright"></i>2021 Dripli. Designed By 
+								<a href="https://envytheme.com/" target="blank">EnvyTheme</a>
+							</p>
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="single-widgets">
+							<ul class="social-icon">
+								<li>
+									<a href="#">
+										<i class="fa fa-facebook"></i>
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="fa fa-twitter"></i>
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="fa fa-youtube-play"></i>
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="fa fa-linkedin"></i>
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="fa fa-instagram"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
---
+++ header.php
@@ -0,0 +1,279 @@
+	<!-- Start Preloader Area -->
+    <div class="preloader">
+			<div class="lds-ripple">
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+        <!-- End Preloader Area -->
+
+		<!-- Start Dripli Navbar Area -->
+		<div class="dripli-header-area fixed-top">
+			<div class="top-header-style-1">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-4">
+							<ul class="contact-info">
+								<li>
+									<a href="#">NEWS & MEDIA</a>
+								</li>
+								<li>
+									<a href="#">CAREERS</a>
+								</li>
+								<li>
+									<a href="#">CONTACTS</a>
+								</li>             
+							</ul>
+						</div>
+						<div class="col-lg-8">
+							<ul class="contact-info contact-info-2">
+								<li>
+									<a class="pointer" href="#">
+										<i class="flaticon-clock-circular-outline"></i>
+										MON - FRI 09:00 - 19:00
+									</a>
+								</li>
+								<li>
+									<a href="tel:+07-554-332-322">
+										<i class="flaticon-telephone-handle-silhouette"></i>
+										CALL US: +07 554 332 322
+									</a>
+								</li>
+								<li>
+									<a href="https://templates.envytheme.com/cdn-cgi/l/email-protection#761e131a1a193612041f061a1f5815191b">
+										<i class="flaticon-mail"></i>
+										<span class="__cf_email__" data-cfemail="e9818c858586a98d9b80998580c78a8684">[email&#160;protected]</span>
+									</a>
+								</li>             
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="navbar-area dripli-nav-style-1">
+				<!-- Menu For Mobile Device -->
+				<div class="mobile-nav">
+					<a href="index.html" class="logo">
+						<img src="assets/img/white-logo.png" alt="Dripli Logo">
+					</a>
+				</div>
+
+				<!-- Menu For Desktop Device -->
+				<div class="main-nav">
+					<nav class="navbar navbar-expand-md navbar-light">
+						<div class="container">
+							<a class="navbar-brand" href="index.html">
+								<img src="assets/img/white-logo.png" alt="Dripli Logo">
+							</a>
+	
+							<div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+								<ul class="navbar-nav m-auto">
+									<li class="nav-item">
+										<a href="#" class="nav-link dropdown-toggle active">Home</a>
+										<ul class="dropdown-menu dropdown-style">
+											<li class="nav-item">
+												<a href="index.html" class="nav-link active">Home One</a>
+											</li>
+											<li class="nav-item">
+												<a href="index-2.html" class="nav-link">Home Two</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-item">
+										<a href="#" class="nav-link dropdown-toggle">Pages</a>
+										<ul class="dropdown-menu dropdown-style">
+											<li class="nav-item">
+												<a href="about.html" class="nav-link">About</a>
+											</li>
+											<li class="nav-item">
+												<a href="#" class="nav-link dropdown-toggle">Services</a>
+												<ul class="dropdown-menu dropdown-style">
+													<li class="nav-item">
+														<a href="services.html" class="nav-link">Services</a>
+													</li>
+													<li class="nav-item">
+														<a href="services-grid.html" class="nav-link">Services Grid</a>
+													</li>
+													<li class="nav-item">
+														<a href="service-details.html" class="nav-link">Service Details</a>
+													</li>
+												</ul>
+											</li>
+											<li class="nav-item">
+												<a href="#" class="nav-link dropdown-toggle">Shop</a>
+												<ul class="dropdown-menu">
+													<li class="nav-item">
+														<a href="shop.html" class="nav-link">Shop</a>
+													</li>
+													<li class="nav-item">
+														<a href="shop-details.html" class="nav-link">Shop Details</a>
+													</li>
+													<li class="nav-item">
+														<a href="checkout.html" class="nav-link">Checkout</a>
+													</li>
+													<li class="nav-item">
+														<a href="cart.html" class="nav-link">Cart</a>
+													</li>
+												</ul>
+											</li>
+											<li class="nav-item">
+												<a href="#" class="nav-link dropdown-toggle">Projects</a>
+												<ul class="dropdown-menu dropdown-style">
+													<li class="nav-item">
+														<a href="projects.html" class="nav-link">Projects</a>
+													</li>
+													<li class="nav-item">
+														<a href="projects-grid.html" class="nav-link">Projects Grid</a>
+													</li>
+													<li class="nav-item">
+														<a href="projects-details.html" class="nav-link">Projects Details</a>
+													</li>
+												</ul>
+											</li>
+											<li class="nav-item">
+												<a href="#" class="nav-link dropdown-toggle">Blog</a>
+												<ul class="dropdown-menu dropdown-style">
+													<li class="nav-item">
+														<a href="blog-grid.html" class="nav-link">Blog Grid</a>
+													</li>
+													<li class="nav-item">
+														<a href="blog-right-sidebar.html" class="nav-link">Blog Right Sidebar</a>
+													</li>
+													<li class="nav-item">
+														<a href="blog-left-sidebar.html" class="nav-link">Blog Left Sidebar</a>
+													</li>
+													<li class="nav-item">
+														<a href="blog-details.html" class="nav-link">Blog Details</a>
+													</li>
+												</ul>
+											</li>
+											<li class="nav-item">
+												<a href="team.html" class="nav-link">Team</a>
+											</li>
+											<li class="nav-item">
+												<a href="faq.html" class="nav-link">FAQ</a>
+											</li>
+											<li class="nav-item">
+												<a href="coming-soon.html" class="nav-link">Coming Soon</a>
+											</li>
+											<li class="nav-item">
+												<a href="404.html" class="nav-link">404 Error</a>
+											</li>
+											<li class="nav-item">
+												<a href="#" class="nav-link dropdown-toggle">User</a>
+												<ul class="dropdown-menu dropdown-style">
+													<li class="nav-item">
+														<a href="log-in.html" class="nav-link">Log In</a>
+													</li>
+													<li class="nav-item">
+														<a href="sign-up.html" class="nav-link">Sign Up</a>
+													</li>
+													<li class="nav-item">
+														<a href="recover-password.html" class="nav-link">Recover Password</a>
+													</li>
+												</ul>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-item">
+										<a href="#" class="nav-link dropdown-toggle">Services</a>
+										<ul class="dropdown-menu dropdown-style">
+											<li class="nav-item">
+												<a href="services.html" class="nav-link">Services</a>
+											</li>
+											<li class="nav-item">
+												<a href="services-grid.html" class="nav-link">Services Grid</a>
+											</li>
+											<li class="nav-item">
+												<a href="service-details.html" class="nav-link">Service Details</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-item">
+										<a href="#" class="nav-link dropdown-toggle">Projects</a>
+										<ul class="dropdown-menu dropdown-style">
+											<li class="nav-item">
+												<a href="projects.html" class="nav-link">Projects</a>
+											</li>
+											<li class="nav-item">
+												<a href="projects-grid.html" class="nav-link">Projects Grid</a>
+											</li>
+											<li class="nav-item">
+												<a href="projects-details.html" class="nav-link">Projects Details</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-item">
+										<a href="#" class="nav-link dropdown-toggle">Shop</a>
+										<ul class="dropdown-menu">
+											<li class="nav-item">
+												<a href="shop.html" class="nav-link">Shop</a>
+											</li>
+											<li class="nav-item">
+												<a href="shop-details.html" class="nav-link">Shop Details</a>
+											</li>
+											<li class="nav-item">
+												<a href="checkout.html" class="nav-link">Checkout</a>
+											</li>
+											<li class="nav-item">
+												<a href="cart.html" class="nav-link">Cart</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-item">
+										<a href="#" class="nav-link dropdown-toggle">Blog</a>
+										<ul class="dropdown-menu dropdown-style">
+											<li class="nav-item">
+												<a href="blog-grid.html" class="nav-link">Blog Grid</a>
+											</li>
+											<li class="nav-item">
+												<a href="blog-right-sidebar.html" class="nav-link">Blog Right Sidebar</a>
+											</li>
+											<li class="nav-item">
+												<a href="blog-left-sidebar.html" class="nav-link">Blog Left Sidebar</a>
+											</li>
+											<li class="nav-item">
+												<a href="blog-details.html" class="nav-link">Blog Details</a>
+											</li>
+										</ul>
+									</li>
+									<li class="nav-item">
+										<a href="contact.html" class="nav-link">Contact</a>
+									</li>
+								</ul>
+								<div class="others-option">
+									<div class="language-list">
+										<div class="select-box">
+											<i class="flaticon-avatar"></i>
+											<select>
+												<option value="1">English</option>
+												<option value="2">العربيّة</option>
+												<option value="3">Germany</option>
+												<option value="4">Português</option>
+												<option value="5">简体中文</option>
+											</select>
+										</div>
+									</div>
+									<a class="cart-icon" href="cart.html">
+										<i class="flaticon-shopping-cart"></i>
+										<span>0</span>
+									</a>
+									<div class="option-item">
+										<i class="search-btn fa fa-search"></i>
+										<i class="close-btn fa fa-close"></i>
+										<div class="search-overlay search-popup">
+											<div class='search-box'>
+												<form class="search-form">
+													<input class="search-input" name="search" placeholder="Search" type="text">
+													<button class="search-button" type="submit"><i class="fa fa-search"></i></button>
+												</form>
+											</div>
+										</div>
+									</div>	
+								</div>
+							</div>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
